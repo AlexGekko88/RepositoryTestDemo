ac005936e92b4385a81ec369bd29eb87c2131872
AlexGekko88 <115148133+AlexGekko88@users.noreply.github.com>
2022-12-27 15:17:48 +0300

    Изменил 5 строчку

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int[] sales = {500,1000,560,12000,2500,3450,8900};
         SalesManager salesManager = new SalesManager(sales);
-        System.out.println("Самая крупная  продажа  " + salesManager.max() + " руб.");
+        System.out.println("Самая крупная  продажа на " + salesManager.max() + " руб.");
     }
 }
